ca6e5c91faa9aaf08a26911d65eb2cafbb3f516b
Victor Dods <victor.dods@gmail.com>
2015-06-15 18:58:47 -0700

    Added symbolic.variable function to return a single named variable.  Really this is to make the function call self-documenting.

--- library/symbolic.py
+++ library/symbolic.py
@@ -1,6 +1,9 @@
 import numpy as np
 import sympy
 
+def variable (name):
+    return sympy.symbols(name)
+
 def tensor (name, shape):
     return np.array(sympy.symbols(name+''.join('(0:{0})'.format(s) for s in shape))).reshape(shape)
 
