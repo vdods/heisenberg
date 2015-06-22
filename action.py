68cad39f7f175bb40fde3faac11d490d91c01d9d
Victor Dods <victor.dods@gmail.com>
2015-06-21 18:02:16 -0700

    Did more work on action.py.

--- action.py
+++ action.py
@@ -9,7 +9,34 @@ import symbolic
 import tensor
 import time
 
-def generate_zeta (M, use_np_einsum):
+def call_func_and_print_timing_info (func_name, func, *args, **kwargs):
+    print 'calling {0} ...'.format(func_name)
+    start = time.time()
+    retval = func(*args, **kwargs)
+    print '{0} took {1} s.'.format(func_name, time.time() - start)
+    return retval
+
+# This and einsum_for_two is from http://stackoverflow.com/questions/15606937/how-do-i-get-numpy-einsum-to-play-well-with-sympy
+def alt_einsum(string, *args):
+    index_groups = map(list, string.split(','))
+    assert len(index_groups) == len(args)
+    tensor_indices_tuples = zip(index_groups, args)
+    return reduce(einsum_for_two, tensor_indices_tuples)[1]
+
+def einsum_for_two(tensor_indices1, tensor_indices2):
+    string1, tensor1 = tensor_indices1
+    string2, tensor2 = tensor_indices2
+    sum_over_indices = set(string1).intersection(set(string2))
+    new_string = string1 + string2
+    axes = ([], [])
+    for i in sum_over_indices:
+        new_string.remove(i)
+        new_string.remove(i)
+        axes[0].append(string1.index(i))
+        axes[1].append(string2.index(i))
+    return new_string, np.tensordot(tensor1, tensor2, axes)
+
+def generate_zeta (M, contraction):
     # TODO: get rid of M_inv if possible
     M_inv = {m:i for i,m in enumerate(M)}
     L = list(frozenset(m1-m2 for m1 in M for m2 in M))
@@ -37,10 +64,12 @@ def generate_zeta (M, use_np_einsum):
     
     def zeta (R):
         assert len(R) == 2*len(M), 'not enough input params.'
-        if use_np_einsum:
+        if contraction == 'np.einsum':
             return np.einsum('ijk,j,k', zeta_tensor, R, R)
-        else:
+        elif contraction == 'tensor.contract':
             return tensor.contract('ijk,j,k', zeta_tensor, R, R, dtype=R.dtype)
+        else:
+            return alt_einsum('ijk,j,k', zeta_tensor, R, R)
     
     return zeta,L,zeta_tensor
 
@@ -55,7 +84,7 @@ def generate_imag_Q (M, period):
 
     return imag_Q
 
-def generate_integrate_over_sample_times (sample_times, period, use_np_einsum):
+def generate_integrate_over_sample_times (sample_times, period, contraction):
     assert len(sample_times) > 0, 'sample_times must be nonempty.'
     assert all(sample_times[i+1] > sample_times[i] for i in range(len(sample_times)-1)), 'sample_times must be a strictly increasing sequence.'
     assert period > sample_times[-1], 'period must be greater than last element of sample_times.'
@@ -63,10 +92,12 @@ def generate_integrate_over_sample_times (sample_times, period, use_np_einsum):
     integral_covector = np.array([sample_times[i+1] - sample_times[i] for i in range(len(sample_times)-1)] + [period - sample_times[-1]])
     assert len(integral_covector) == len(sample_times)
     def integrate_over_sample_times (X):
-        if use_np_einsum:
+        if contraction == 'np.einsum':
             return np.einsum('j,j', integral_covector, X)
-        else:
+        elif contraction == 'tensor.contract':
             return tensor.contract('j,j', integral_covector, X, dtype=X.dtype)
+        else:
+            return alt_einsum('j,j', integral_covector, X)
     return integrate_over_sample_times
 
 def generate_imag_projection_over_sample_times (sample_times):
@@ -94,28 +125,33 @@ def eta ((U,V)):
     retval[:,n:] = V
     return retval
 
-alpha = 2.0/math.pi
-beta = 16.0
+def generate_hamiltonian (alpha, beta):
+    def hamiltonian (pv):
+        assert len(pv) == 6
+        mu = (pv[0]**2 + pv[1]**2)**2 + beta*pv[2]**2
+        # First term is kinetic energy, second is potential.
+        return 0.5*(pv[3]**2 + pv[4]**2) - alpha*mu**(-0.5)
+    return hamiltonian
 
-def hamiltonian (pv):
-    assert len(pv) == 6
-    mu = (pv[0]**2 + pv[1]**2)**2 + beta*pv[2]**2
-    # First term is kinetic energy, second is potential.
-    return 0.5*(pv[3]**2 + pv[4]**2) - alpha*mu**(-0.5)
+def generate_hamiltonian_v (hamiltonian):
+    def hamiltonian_v (PV):
+        assert PV.shape[1] == 6
+        return np.array([hamiltonian(pv) for pv in PV])
+    return hamiltonian_v
 
-def hamiltonian_v (PV):
-    assert PV.shape[1] == 6
-    return np.array([hamiltonian(pv) for pv in PV])
+def generate_lagrangian (alpha, beta):
+    def lagrangian (pv):
+        assert len(pv) == 6
+        mu = (pv[0]**2 + pv[1]**2)**2 + beta*pv[2]**2
+        # First term is kinetic energy, second is potential.
+        return 0.5*(pv[3]**2 + pv[4]**2) + alpha*mu**(-0.5)
+    return lagrangian
 
-def lagrangian (pv):
-    assert len(pv) == 6
-    mu = (pv[0]**2 + pv[1]**2)**2 + beta*pv[2]**2
-    # First term is kinetic energy, second is potential.
-    return 0.5*(pv[3]**2 + pv[4]**2) + alpha*mu**(-0.5)
-
-def lagrangian_v (PV):
-    assert PV.shape[1] == 6
-    return np.array([lagrangian(pv) for pv in PV])
+def generate_lagrangian_v (lagrangian):
+    def lagrangian_v (PV):
+        assert PV.shape[1] == 6
+        return np.array([lagrangian(pv) for pv in PV])
+    return lagrangian_v
 
 def load_cache_or_compute (cache_filename, computation):
     import pickle
@@ -148,9 +184,13 @@ class ProblemContext:
             xy_mode_count   : The number of modes used in the Fourier sum for the xy curve.
             sample_count    : A positive integer indicating how many samples will be used to represent the xy curve.
             period          : A positive float indicating the period of the curve.
-            use_np_einsum   : A boolean indicating if np.einsum should be used instead of tensor.contract.  The default is True.
+            contraction     : Specifies the method used to contract tensors.  One of: 'np.einsum', 'tensor.contract',
+                              'alt_einsum'.  Note that dtype=object can't use np.einsum (for some dumb reason).
+                              Default is 'np.einsum', because it is the fastest.
+            alpha           : The alpha parameter of the Hamiltonian.
+            beta            : The beta parameter of the Hamiltonian.
         """
-        self.parameter_string = repr(kwargs)
+        self.parameter_string = repr(sorted(kwargs.iteritems()))
         
         self.symmetry_degree = kwargs['symmetry_degree']
         self.symmetry_class = kwargs['symmetry_class']
@@ -173,49 +213,66 @@ class ProblemContext:
         assert all(self.sample_times[i+1] > self.sample_times[i] for i in range(len(self.sample_times)-1)), 'sample_times must be a strictly increasing sequence.'
         assert self.period > self.sample_times[-1], 'period must be greater than last element of sample_times.'
 
-        self.use_np_einsum = kwargs.get('use_np_einsum', True)
+        self.contraction = kwargs.get('contraction', 'np.einsum')
+        assert self.contraction in ['np.einsum', 'tensor.contract', 'alt_einsum']
 
-        start = time.time(); self.zeta_M,self.wz_modes,self.zeta_tensor = generate_zeta(self.xy_modes, self.use_np_einsum)#; print 'generate_zeta: {0} s'.format(time.time() - start)
+        self.alpha = kwargs['alpha']
+        self.beta = kwargs['beta']
+
+        start = time.time(); self.zeta_M,self.wz_modes,self.zeta_tensor = generate_zeta(self.xy_modes, self.contraction)#; print 'generate_zeta: {0} s'.format(time.time() - start)
+        print 'len(sample_times) = {0}, len(xy_modes) = {1}, len(wz_modes) = {2}'.format(len(self.sample_times), len(self.xy_modes), len(self.wz_modes))
         start = time.time(); self.F_xy = realified_fourier.Transform(self.xy_modes, self.sample_times, self.period)#; print 'F_xy: {0} s'.format(time.time() - start)
         start = time.time(); self.F_wz = realified_fourier.Transform(self.wz_modes, self.sample_times, self.period)#; print 'F_wz: {0} s'.format(time.time() - start)
         start = time.time(); self.imag_projection_over_sample_times,self.imag_projection_matrix = generate_imag_projection_over_sample_times(self.sample_times)#; print 'generate_imag_projection_over_sample_times: {0} s'.format(time.time() - start)
         start = time.time(); self.imag_Q = generate_imag_Q(self.xy_modes, self.F_xy.omega)#; print 'generate_imag_Q: {0} s'.format(time.time() - start)
-        start = time.time(); self.integrate_over_sample_times = generate_integrate_over_sample_times(self.sample_times, self.period, self.use_np_einsum)#; print 'generate_integrate_over_sample_times: {0} s'.format(time.time() - start)
+        start = time.time(); self.integrate_over_sample_times = generate_integrate_over_sample_times(self.sample_times, self.period, self.contraction)#; print 'generate_integrate_over_sample_times: {0} s'.format(time.time() - start)
 
         def xy_curve (R):
-            if self.use_np_einsum:
+            if self.contraction == 'np.einsum':
                 return np.einsum('ij,j', self.F_xy.samples_from_coeffs_matrix, R)
-            else:
+            elif self.contraction == 'tensor.contract':
                 return tensor.contract('ij,j', self.F_xy.samples_from_coeffs_matrix, R, dtype=R.dtype)
+            else:
+                return alt_einsum('ij,j', self.F_xy.samples_from_coeffs_matrix, R)
 
         # start = time.time(); z_curve_tensor = np.einsum('ij,jk,klm', self.imag_projection_matrix, self.F_wz.samples_from_coeffs_matrix, self.zeta_tensor); print 'z_curve_tensor (with shape {0}): {1} s'.format(z_curve_tensor.shape, time.time() - start)
         start = time.time(); z_curve_tensor = np.einsum('ij,jkl', np.einsum('ij,jk', self.imag_projection_matrix, self.F_wz.samples_from_coeffs_matrix), self.zeta_tensor); print 'z_curve_tensor (with shape {0}): {1} s'.format(z_curve_tensor.shape, time.time() - start)
         def z_curve (R):
-            if self.use_np_einsum:
+            if self.contraction == 'np.einsum':
                 return np.einsum('ijk,j,k', z_curve_tensor, R, R) + self.imag_Q(R)*self.sample_times
-            else:
+            elif self.contraction == 'tensor.contract':
                 return tensor.contract('ijk,j,k', z_curve_tensor, R, R, dtype=R.dtype) + self.imag_Q(R)*self.sample_times
+            else:
+                return alt_einsum('ijk,j,k', z_curve_tensor, R, R) + self.imag_Q(R)*self.sample_times
 
         start = time.time(); xy_prime_curve_matrix = np.einsum('ij,jk', self.F_xy.samples_from_coeffs_matrix, self.F_xy.time_derivative_matrix); print 'xy_prime_curve_matrix (with shape {0}): {1} s'.format(xy_prime_curve_matrix.shape, time.time() - start)
         def xy_prime_curve (R):
-            if self.use_np_einsum:
+            if self.contraction == 'np.einsum':
                 return np.einsum('ij,j', xy_prime_curve_matrix, R)
-            else:
+            elif self.contraction == 'tensor.contract':
                 return tensor.contract('ij,j', xy_prime_curve_matrix, R, dtype=R.dtype)
+            else:
+                return alt_einsum('ij,j', xy_prime_curve_matrix, R)
 
         # start = time.time(); z_prime_curve_tensor = np.einsum('ij,jk,kl,lmn', self.imag_projection_matrix, self.F_wz.samples_from_coeffs_matrix, self.F_wz.time_derivative_matrix, self.zeta_tensor); print 'z_prime_curve_tensor (with shape {0}): {1} s'.format(z_prime_curve_tensor.shape, time.time() - start)
         start = time.time(); z_prime_curve_tensor = np.einsum('ij,jkl', np.einsum('ij,jk', np.einsum('ij,jk', self.imag_projection_matrix, self.F_wz.samples_from_coeffs_matrix), self.F_wz.time_derivative_matrix), self.zeta_tensor); print 'z_prime_curve_tensor (with shape {0}): {1} s'.format(z_prime_curve_tensor.shape, time.time() - start)
         vector_of_ones = np.array([1.0 for _ in self.sample_times])
         def z_prime_curve (R):
-            if self.use_np_einsum:
+            if self.contraction == 'np.einsum':
                 return np.einsum('ijk,j,k', z_prime_curve_tensor, R, R) + self.imag_Q(R)*vector_of_ones
-            else:
+            elif self.contraction == 'tensor.contract':
                 return tensor.contract('ijk,j,k', z_prime_curve_tensor, R, R, dtype=R.dtype) + self.imag_Q(R)*vector_of_ones
+            else:
+                return alt_einsum('ijk,j,k', z_prime_curve_tensor, R, R) + self.imag_Q(R)*vector_of_ones
 
         self.position = lambda R : chi((xy_curve(R), z_curve(R)))
         self.velocity = lambda R : chi((xy_prime_curve(R), z_prime_curve(R)))
         self.position_and_velocity = lambda R : eta((self.position(R), self.velocity(R)))
-        self.action = lambda R : self.integrate_over_sample_times(lagrangian_v(self.position_and_velocity(R)))
+        self.lagrangian = generate_lagrangian(self.alpha, self.beta)
+        self.lagrangian_v = generate_lagrangian_v(self.lagrangian)
+        self.hamiltonian = generate_hamiltonian(self.alpha, self.beta)
+        self.hamiltonian_v = generate_hamiltonian_v(self.hamiltonian)
+        self.action = lambda R : self.integrate_over_sample_times(self.lagrangian_v(self.position_and_velocity(R)))
         self.Lambda = lambda R_lagmult : self.action(R_lagmult[:-1]) + R_lagmult[-1]*self.imag_Q(R_lagmult[:-1])
 
     def generate_symbolic_functions (self):
@@ -228,7 +285,7 @@ class ProblemContext:
         def compute_diff_and_print_progress (f, var, i, out_of):
             sys.stdout.write('computing {0}th derivative out of {1} ... '.format(i, out_of))
             retval = f.diff(var)
-            sys.stdout.write('complete.')
+            sys.stdout.write('complete.\n')
             return retval
 
         self.R_lagmult_vars = load_cache_or_compute('cache/R_lagmult_vars.{0}.pickle'.format(self.parameter_string), generate_variables)
@@ -249,17 +306,38 @@ class ProblemContext:
         self.objective_function = sympy.utilities.autowrap.autowrap(self.symbolic_squared_L2_norm_D_Lambda, args=self.R_lagmult_vars, backend='cython')
         print 'generating objective_function (via autowrap) took {0} s.'.format(time.time() - start)
 
+def profile_ProblemContext ():
+    symmetry_degree = 5
+    symmetry_class = 2
+    xy_mode_count_range = range(2,3+1)#range(2,10)
+    sample_count_range = range(21,22+1)#range(16,48,8)
+    period = 46.5
+    alpha = 1.0
+    beta = 16.0
+    contractions = ['tensor.contract', 'alt_einsum']
+    for contraction in contractions:
+        for sample_count in sample_count_range:
+            for xy_mode_count in xy_mode_count_range:
+                try:
+                    print '****************** contraction = {0}, sample_count = {1}, xy_mode_count = {2}'.format(contraction, sample_count, xy_mode_count)
+                    pc = call_func_and_print_timing_info('ProblemContext', ProblemContext, symmetry_degree=symmetry_degree, symmetry_class=symmetry_class, xy_mode_count=xy_mode_count, sample_count=sample_count, period=period, alpha=alpha, beta=beta, contraction=contraction)
+                    call_func_and_print_timing_info('pc.generate_symbolic_functions', pc.generate_symbolic_functions)
+                    call_func_and_print_timing_info('pc.generate_autowrapped_functions', pc.generate_autowrapped_functions)
+                except Exception as e:
+                    print 'caught exception {0}'.format(repr(e))
+                print ''
+                print ''
+
 def main ():
     import scipy.optimize
     import sympy.utilities.autowrap
 
-    start = time.time()
-    # pc = ProblemContext(symmetry_degree=3, symmetry_class=1, xy_mode_count=7, sample_count=100, period=10.0, use_np_einsum=False)
-    pc = ProblemContext(symmetry_degree=3, symmetry_class=1, xy_mode_count=10, sample_count=100, period=10.0, use_np_einsum=False)
-    print 'ProblemContext generated in {0} s.'.format(time.time() - start)
+    # pc = ProblemContext(symmetry_degree=3, symmetry_class=1, xy_mode_count=7, sample_count=100, period=10.0, contraction='alt_einsum')
+    # pc = ProblemContext(symmetry_degree=5, symmetry_class=2, xy_mode_count=10, sample_count=200, period=46.5, alpha=1.0, beta=16.0, contraction='alt_einsum')
+    pc = call_func_and_print_timing_info('ProblemContext', ProblemContext, symmetry_degree=5, symmetry_class=2, xy_mode_count=4, sample_count=30, period=46.5, alpha=1.0, beta=16.0, contraction='tensor.contract')
 
-    pc.generate_symbolic_functions()
-    pc.generate_autowrapped_functions()
+    call_func_and_print_timing_info('pc.generate_symbolic_functions', pc.generate_symbolic_functions)
+    call_func_and_print_timing_info('pc.generate_autowrapped_functions', pc.generate_autowrapped_functions)
     
     # ---------------------------------------
 
@@ -283,8 +361,8 @@ def main ():
 
     print 'action(R) = {0}'.format(pc.action(R))
 
-    H = hamiltonian_v(PV)
-    L = lagrangian_v(PV)
+    H = pc.hamiltonian_v(PV)
+    L = pc.lagrangian_v(PV)
 
     import matplotlib.pyplot as plt
 
