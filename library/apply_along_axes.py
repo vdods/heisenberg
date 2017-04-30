bebbb8a4de08f4250ea07c90de5b242615da6deb
Victor Dods <victor.dods@gmail.com>
2017-04-30 16:01:53 -0700

    Made it so apply_along_axes can accept negative axis indices, just like numpy.apply_along_axis.

--- library/apply_along_axes.py
+++ library/apply_along_axes.py
@@ -139,9 +139,13 @@ def apply_along_axes (func, input_axis_v, input_array, *args, output_axis_v=None
     """
     This function is a generalization of numpy.apply_along_axis.
 
-    Let A = (0,1,...,N-1), where N is len(input_array.shape).  input_axis_v should be a nonempty
+    Let A = (0,1,...,N-1), where N is len(input_array.shape).  input_axis_v should be* a nonempty
     subsequence of A; this means that 0 < len(input_axis_v) <= len(A), each element of input_axis_v
     must be an element of the sequence A, and input_axis_v must be strictly increasing (as A is).
+
+    *Note that input_axis_v may contain values in the range [-N,0), in which case, N is added to
+    each negative element to bring them all within the range [0,N) as valid axis indices.  For example,
+    this means that -1 addresses the last axis, -2 addresses the second-to-last axis, etc.
 
     TODO: Allow the base case where input_array.shape = () and input_axis_v = [].
 
@@ -158,10 +162,13 @@ def apply_along_axes (func, input_axis_v, input_array, *args, output_axis_v=None
     specified above).  Let B = (0,1,...,M-1), where M is len(I)+len(S).  The return value of
     apply_along_axes will be a numpy.ndarray having M indices.
 
-    If output_axis_v is not None, it must be a subsequence of B, specifying which axes of the
+    If output_axis_v is not None, it must be** a subsequence of B, specifying which axes of the
     return value will be used to index the output of func.  If output_axis_v is None, then it
     will be assumed to have value (L,L+1,...,M-1), where L = len(I), i.e. the output axes will
     be the last axes of the return value.
+
+    **Note that, just as input_axis_v, output_axis_v may contain values in the range [-N,0), in which
+    case, N is added to bring each negative element to bring them all within the range [0,N).
 
     All extra args and kwargs will be passed through to calls to func.
 
@@ -180,48 +187,56 @@ def apply_along_axes (func, input_axis_v, input_array, *args, output_axis_v=None
     A = np.arange(N)
     # Note that the length of the complementary subsequence of N in A is N-len(input_axis_v).
 
+    assert all(-N <= input_axis < N for input_axis in input_axis_v), 'input_axis_v (which is {0}), must contain values in the range of [-N,N) (where N = {1})'.format(input_axis_v, N)
+
+    normalized_input_axis_v = np.fromiter((input_axis if input_axis >= 0 else input_axis+N for input_axis in input_axis_v), np.int, len(input_axis_v))
+
     # print('input_array.shape = {0}'.format(input_array.shape))
 
-    assert __is_subsequence_of_nondecreasing_sequence(input_axis_v, A), 'input_axis_v (which is {0}), must be a nonempty subsequence of (0,1,...,{1})'.format(input_axis_v, len(input_array.shape)-1)
+    assert __is_subsequence_of_nondecreasing_sequence(normalized_input_axis_v, A), 'normalized_input_axis_v (which is {0}) must be a nonempty subsequence of [0,{1}) (right endpoint excluded)'.format(normalized_input_axis_v, len(input_array.shape))
 
-    is_not_input_axis_v = np.array([axis_index not in input_axis_v for axis_index in A])
-    non_input_axis_v = np.array([axis_index for axis_index in A if is_not_input_axis_v[axis_index]])
-    input_iterator_v = [range(input_array.shape[axis_index]) if is_not_input_axis_v[axis_index] else [slice(None)] for axis_index in A]
-    # print('is_not_input_axis_v = {0}'.format(is_not_input_axis_v))
-    # print('non_input_axis_v = {0}'.format(non_input_axis_v))
+    is_not_normalized_input_axis_v = np.array([axis_index not in normalized_input_axis_v for axis_index in A])
+    non_normalized_input_axis_v = np.array([axis_index for axis_index in A if is_not_normalized_input_axis_v[axis_index]])
+    input_iterator_v = [range(input_array.shape[axis_index]) if is_not_normalized_input_axis_v[axis_index] else [slice(None)] for axis_index in A]
+    # print('is_not_normalized_input_axis_v = {0}'.format(is_not_normalized_input_axis_v))
+    # print('non_normalized_input_axis_v = {0}'.format(non_normalized_input_axis_v))
     # print('input_iterator_v = {0}'.format(input_iterator_v))
 
     # If func_output_shape is not specified, then derive it.
     if func_output_shape is None:
-        func_output_shape = np.shape(func(input_array[tuple(0 if axis_index not in input_axis_v else slice(None) for axis_index in A)], *args, **kwargs))
+        func_output_shape = np.shape(func(input_array[tuple(0 if axis_index not in normalized_input_axis_v else slice(None) for axis_index in A)], *args, **kwargs))
     # print('func_output_shape = {0}'.format(func_output_shape))
 
-    # B = np.arange(N-len(input_axis_v)+len(func_output_shape))
-    B = np.arange(len(non_input_axis_v)+len(func_output_shape))
+    # B = np.arange(N-len(normalized_input_axis_v)+len(func_output_shape))
+    B = np.arange(len(non_normalized_input_axis_v)+len(func_output_shape))
+    M = len(B)
     # print('B = {0}'.format(B))
 
     # If output_axis_v is None, then it will be assumed to be the trailing axes of the output.
     if output_axis_v is None:
-        output_axis_v = np.arange(len(B)-len(func_output_shape), len(B))
-    # print('input_axis_v = {0}'.format(input_axis_v))
+        output_axis_v = np.arange(-len(func_output_shape), 0)
+    # print('normalized_input_axis_v = {0}'.format(normalized_input_axis_v))
     # print('output_axis_v = {0}'.format(output_axis_v))
 
     assert len(func_output_shape) == len(output_axis_v), 'func_output_shape (which is {0}) must have same number of elements as output_axis_v (which is {1})'.format(func_output_shape, output_axis_v)
-    assert __is_subsequence_of_nondecreasing_sequence(output_axis_v, B), 'output_axis_v (which is {0}), must be a subsequence of [0,...,{1}) (right endpoint excluded)'.format(output_axis_v, len(B))
 
-    is_not_output_axis_v = [axis_index not in output_axis_v for axis_index in B]
+    normalized_output_axis_v = np.fromiter((output_axis if output_axis >= 0 else output_axis+M for output_axis in output_axis_v), np.int, len(output_axis_v))
+
+    assert __is_subsequence_of_nondecreasing_sequence(normalized_output_axis_v, B), 'normalized_output_axis_v (which is {0}) must be a subsequence of [0,{1}) (right endpoint excluded)'.format(normalized_output_axis_v, len(B))
+
+    is_not_output_axis_v = [axis_index not in normalized_output_axis_v for axis_index in B]
     # print('is_not_output_axis_v = {0}'.format(is_not_output_axis_v))
     non_output_axis_v = [axis_index for axis_index in B if is_not_output_axis_v[axis_index]]
     # print('non_output_axis_v = {0}'.format(non_output_axis_v))
     non_output_axis_inv_v = __index_map_inverse(non_output_axis_v, len(B))
     # print('non_output_axis_inv_v = {0}'.format(non_output_axis_inv_v))
-    output_axis_inv_v = __index_map_inverse(output_axis_v, len(B))
+    output_axis_inv_v = __index_map_inverse(normalized_output_axis_v, len(B))
     # print('output_axis_inv_v = {0}'.format(output_axis_inv_v))
-    # print('__compose_index_maps(non_input_axis_v, non_output_axis_inv_v) = {0}'.format(__compose_index_maps(non_input_axis_v, non_output_axis_inv_v)))
-    output_iterator_v = [range(input_array.shape[non_input_axis_v[non_output_axis_inv_v[axis_index]]]) if is_not_output_axis_v[axis_index] else [slice(None)] for axis_index in B]
+    # print('__compose_index_maps(non_normalized_input_axis_v, non_output_axis_inv_v) = {0}'.format(__compose_index_maps(non_normalized_input_axis_v, non_output_axis_inv_v)))
+    output_iterator_v = [range(input_array.shape[non_normalized_input_axis_v[non_output_axis_inv_v[axis_index]]]) if is_not_output_axis_v[axis_index] else [slice(None)] for axis_index in B]
     # print('output_iterator_v = {0}'.format(output_iterator_v))
 
-    retval = np.ndarray(tuple(input_array.shape[non_input_axis_v[non_output_axis_inv_v[axis_index]]] if is_not_output_axis_v[axis_index] else func_output_shape[output_axis_inv_v[axis_index]] for axis_index in B), dtype=input_array.dtype)
+    retval = np.ndarray(tuple(input_array.shape[non_normalized_input_axis_v[non_output_axis_inv_v[axis_index]]] if is_not_output_axis_v[axis_index] else func_output_shape[output_axis_inv_v[axis_index]] for axis_index in B), dtype=input_array.dtype)
     # print('retval.shape = {0}'.format(retval.shape))
     for input_I,output_I in zip(itertools.product(*input_iterator_v), itertools.product(*output_iterator_v)):
         # print('input_I = {0}, output_I = {1}'.format(input_I, output_I))
@@ -248,26 +263,36 @@ def __test__apply_along_axes ():
 
     rng = np.random.RandomState(42)
     a = rng.randn(2,3,4,5)
+    N = len(a.shape)
 
     # Use all possible combinations of input and output axes.
-    for input_i0 in range(3):
-        for input_i1 in range(input_i0+1,4):
-            for output_i0 in range(3):
-                for output_i1 in range(output_i0+1,4):
+    for input_i0 in range(-N,N-1):
+        for input_i1 in range(input_i0+1,N):
+            # Only test the pairs where input_i0 indexes an axis before that indexed by input_i1.
+            if input_i0 + N >= input_i1:
+                continue
+
+            for output_i0 in range(-N,N-1):
+                for output_i1 in range(output_i0+1,N):
+                    # Only test the pairs where output_i0 indexes an axis before that indexed by output_i1.
+                    if output_i0 + N >= output_i1:
+                        continue
+
+                    output_axis_v = (output_i0,output_i1)
                     # Compute the result.  The multi-slice across the output axes should be a symmetric matrix.
-                    result = apply_along_axes(symmetric_square, (input_i0,input_i1), a, output_axis_v=(output_i0,output_i1))
-                    # Figure out which indices correspond to the input axes; call these result_non_output_indices.
-                    all_indices = (0,1,2,3)
-                    output_indices = (output_i0,output_i1)
-                    result_non_output_indices = sorted(list(frozenset(all_indices) - frozenset(output_indices)))
-                    # print('output_indices = {0}, result_non_output_indices = {1}'.format(output_indices, result_non_output_indices))
-                    assert len(result_non_output_indices) == 2
+                    result = apply_along_axes(symmetric_square, (input_i0,input_i1), a, output_axis_v=output_axis_v)
+                    # Figure out which indices correspond to the input axes; call these result_non_output_axis_v.
+                    all_indices = tuple(range(N))
+                    normalized_output_axis_v = tuple(output_axis if output_axis >= 0 else output_axis+N for output_axis in output_axis_v)
+                    result_non_output_axis_v = sorted(list(frozenset(all_indices) - frozenset(normalized_output_axis_v)))
+                    # print('output_axis_v = {0}, result_non_output_axis_v = {1}'.format(output_axis_v, result_non_output_axis_v))
+                    assert len(result_non_output_axis_v) == 2
                     # Take all multi-slices and verify symmetry.
-                    for check_i0,check_i1 in itertools.product(range(result.shape[result_non_output_indices[0]]), range(result.shape[result_non_output_indices[1]])):
+                    for check_i0,check_i1 in itertools.product(range(result.shape[result_non_output_axis_v[0]]), range(result.shape[result_non_output_axis_v[1]])):
                         # Construct the multi-slice to take.
                         multislice = [slice(None) for _ in range(4)]
-                        multislice[result_non_output_indices[0]] = check_i0
-                        multislice[result_non_output_indices[1]] = check_i1
+                        multislice[result_non_output_axis_v[0]] = check_i0
+                        multislice[result_non_output_axis_v[1]] = check_i1
                         # print('multislice = {0}'.format(multislice))
                         assert is_symmetric(result[multislice])
 
