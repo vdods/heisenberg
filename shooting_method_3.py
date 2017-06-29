146fc5734529d31d46be0c75619dfc40b77bfa85
Victor Dods <victor.dods@gmail.com>
2017-06-29 16:38:36 -0700

    Took momentum out of plot to clean some stuff up.

--- shooting_method_3.py
+++ shooting_method_3.py
@@ -569,7 +569,7 @@ def evaluate_shooting_method_objective (dynamics_context, qp_0, t_max, t_delta):
 class OrbitPlot:
     def __init__ (self, *, row_count, extra_col_count):
         row_count = row_count
-        col_count = 7+extra_col_count
+        col_count = 5+extra_col_count
         self.fig,self.axis_vv = plt.subplots(row_count, col_count, squeeze=False, figsize=(15*col_count,15*row_count))
 
     def plot_curve (self, *, curve_description, axis_v, smo):
@@ -577,7 +577,9 @@ class OrbitPlot:
         H_v = vorpy.apply_along_axes(HeisenbergDynamicsContext_Numeric.H, (-2,-1), (flow_curve,), output_axis_v=(), func_output_shape=())
         abs_H_v = np.abs(H_v)
 
-        axis = axis_v[0]
+        axis_index = 0
+
+        axis = axis_v[axis_index]
         axis.set_title('{0} curve xy-position'.format(curve_description))
         axis.plot(0, 0, 'o', color='black')
         axis.plot(flow_curve[:,0,0], flow_curve[:,0,1])
@@ -585,42 +587,51 @@ class OrbitPlot:
         # TODO: Plot the interpolated position/momentum (based on smo.t_min() instead of Q_global_min_index)
         axis.plot(flow_curve[smo.Q_global_min_index(),0,0], flow_curve[smo.Q_global_min_index(),0,1], 'o', color='red', alpha=0.5)
         axis.set_aspect('equal')
+        axis_index += 1
 
-        axis = axis_v[1]
-        axis.set_title('{0} curve z-position'.format(curve_description))
+        axis = axis_v[axis_index]
+        axis.set_title('{0} curve z-position (green line indicates t_min)'.format(curve_description))
         axis.axhline(0, color='black')
         axis.plot(smo.t_v(), flow_curve[:,0,2])
+        axis.axvline(smo.t_min(), color='green')
+        axis_index += 1
 
-        axis = axis_v[2]
-        axis.set_title('{0} curve xy-momentum'.format(curve_description))
-        axis.plot(flow_curve[:,1,0], flow_curve[:,1,1])
-        axis.plot(flow_curve[0,1,0], flow_curve[0,1,1], 'o', color='green', alpha=0.5)
-        # TODO: Plot the interpolated position/momentum (based on smo.t_min() instead of Q_global_min_index)
-        axis.plot(flow_curve[smo.Q_global_min_index(),1,0], flow_curve[smo.Q_global_min_index(),1,1], 'o', color='red', alpha=0.5)
-        axis.set_aspect('equal')
+        #axis = axis_v[axis_index]
+        #axis.set_title('{0} curve xy-momentum'.format(curve_description))
+        #axis.plot(flow_curve[:,1,0], flow_curve[:,1,1])
+        #axis.plot(flow_curve[0,1,0], flow_curve[0,1,1], 'o', color='green', alpha=0.5)
+        ## TODO: Plot the interpolated position/momentum (based on smo.t_min() instead of Q_global_min_index)
+        #axis.plot(flow_curve[smo.Q_global_min_index(),1,0], flow_curve[smo.Q_global_min_index(),1,1], 'o', color='red', alpha=0.5)
+        #axis.set_aspect('equal')
+        #axis_index += 1
 
-        axis = axis_v[3]
-        axis.set_title('{0} curve z-momentum'.format(curve_description))
-        axis.axhline(0, color='black')
-        axis.plot(smo.t_v(), flow_curve[:,1,2])
+        #axis = axis_v[axis_index]
+        #axis.set_title('{0} curve z-momentum'.format(curve_description))
+        #axis.axhline(0, color='black')
+        #axis.plot(smo.t_v(), flow_curve[:,1,2])
+        #axis.axvline(smo.t_min(), color='green')
+        #axis_index += 1
 
-        axis = axis_v[4]
+        axis = axis_v[axis_index]
         axis.set_title('abs(H) (should stay close to 0)\nmax(abs(H)) = {0:.2e}, H_0 = {1:e}'.format(np.max(abs_H_v), H_v[0]))
         axis.semilogy(smo.t_v(), abs_H_v)
+        axis_index += 1
 
         J_v = vorpy.apply_along_axes(HeisenbergDynamicsContext_Numeric.J, (-2,-1), (flow_curve,), output_axis_v=(), func_output_shape=())
         J_0 = J_v[0]
         J_v -= J_0
         abs_J_minus_J_0 = np.abs(J_v)
 
-        axis = axis_v[5]
+        axis = axis_v[axis_index]
         axis.set_title('abs(J - J_0) (should be close to 0)\nJ_0 = {0}; max(abs(J - J_0)) = {1:.2e}'.format(J_0, np.max(abs_J_minus_J_0)))
         axis.semilogy(smo.t_v(), abs_J_minus_J_0)
+        axis_index += 1
 
-        axis = axis_v[6]
+        axis = axis_v[axis_index]
         axis.set_title('squared distance to initial condition\nt_min = {0}, min sqd = {1:.17e}'.format(smo.t_min(), smo.objective()))
         axis.semilogy(smo.t_v(), smo.squared_distance_function())
         axis.axvline(smo.t_min(), color='green')
+        axis_index += 1
 
     def plot_and_clear (self, *, filename):
         self.fig.tight_layout()
