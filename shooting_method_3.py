ed57ba3352c3aeccc122b586b95bb052d0e016df
Victor Dods <victor.dods@gmail.com>
2017-07-06 23:37:43 -0700

    Added --sample mode where random points in a bounded domain in the 2D parameter space are generated, the initial conditions used to integrate curves, and then statistics about those curves are computed and stored in pickle files.

--- shooting_method_3.py
+++ shooting_method_3.py
@@ -1,8 +1,11 @@
 import abc
+import collections
 import itertools
 import library.monte_carlo
 import numpy as np
+import os
 import sympy as sp
+import sys
 import time
 import vorpy
 import vorpy.pickle
@@ -34,6 +37,8 @@ Define R_Omega to give point closest to Omega(q,p).  Then f_Omega is defined as
 and the gradient of f_Omega depends on the gradient of Omega and R_Omega.
 
 TODO
+-   Change the search mechanism for increasing time such that no redundant integration is done, but
+    simply the integrator continues at the time it left off.
 -   In addition to plot and pickle files, output a short, human-readable summary of the pickle file,
     most importantly with the exact initial conditions, dt, t_min, and the values for alpha and beta.
 -   Examine following hypothesis: Z curve for closed orbit is a sine wave.
@@ -73,11 +78,11 @@ TODO
     -   For each flow curve, compute the following values:
         -   The objective function value (which could be NaN if the curve didn't go back toward itself)
         -   The t_min value (which for a closed curve would be its period)
-        -   The upper bound on abs(H)
-        -   The upper bound on abs(J - J_0)
+        -   The upper bound on abs(H), or more detailed stats (mean and variance, or a histogram)
+        -   The upper bound on abs(J - J_0), or more detailed stats (mean and variance, or a histogram)
         -   If it is [close to being] closed, then the order of its radial symmetry
-        -   The deviation of its z(t) function from a sine wave (for closed, this always appears to
-            be a sine wave, but this hasn't been checked).
+        -   The deviation of its z(t) function from a sine wave (for closed, this sometimes but not
+            always appears to be a sine wave, but this hasn't been checked rigorously).
     -   Visualize these two functions to determine if they are continuous and/or have other structure
         to them.  Perhaps there are regions which are continuous surrounding zeros the objective function.
 """
@@ -866,6 +871,13 @@ class OptionParser:
             help='Indicates that random initial conditions should be generated and for each, if a threshold is met, an optimization routine run to attempt to close the orbit.'
         )
         self.op.add_option(
+            '--sample',
+            dest='sample',
+            action='store_true',
+            default=False,
+            help='Indefinitely generates random points in the 2D initial condition space, integrates the curves with the corresponding initial conditions, then records the various statistics for each of them, saving all stats in a pickle file upon program exit (e.g. via KeyboardInterrupt or other exception).'
+        )
+        self.op.add_option(
             '--k-fold-initial',
             dest='k',
             type='int',
@@ -896,6 +908,8 @@ class OptionParser:
 
         if options.search:
             require_initial_conditions = False
+        elif options.sample:
+            require_initial_conditions = False
         elif options.k is not None:
             require_initial_conditions = False
             options.qp_0 = np.array([
@@ -913,7 +927,7 @@ class OptionParser:
         ])
         if require_initial_conditions:
             if num_initial_conditions_specified != 1:
-                print('if neither --search nor --k-fold-initial are not specified, then you must specify exactly one of --initial-2preimage or --initial-3preimage or --initial-5preimage or --initial, but {0} of those were specified.'.format(num_initial_conditions_specified))
+                print('if none of --search, --sample, --k-fold-initial are specified, then you must specify exactly one of --initial-2preimage or --initial-3preimage or --initial-5preimage or --initial, but {0} of those were specified.'.format(num_initial_conditions_specified))
                 self.op.print_help()
                 return None,None
 
@@ -997,7 +1011,17 @@ def ndarray_as_single_line_string (A):
 def construct_base_filename (*, obj, t_delta, t_max, initial_condition, t_min):
     return 'obj:{0:.4e}.dt:{1:.3e}.t_max:{2:.3e}.ic:{3}.t_min:{4:.4e}'.format(obj, t_delta, t_max, ndarray_as_single_line_string(initial_condition), t_min)
 
-def search (dynamics_context, options):
+def random_embedding2_point (rng):
+    C = 2.0
+    #epsilon = 1.0e-5
+    epsilon = 0.0
+    # Perturb the bounds for p_x by epsilon away from the actual bound.
+    return np.array([
+        rng.uniform(-np.sqrt(4/np.pi)+epsilon, np.sqrt(4/np.pi)-epsilon),
+        rng.uniform(0.0, C)
+    ])
+
+def search (dynamics_context, options, *, rng):
     if not os.path.exists('shooting_method_3/'):
         os.mkdir('shooting_method_3')
 
@@ -1018,14 +1042,15 @@ def search (dynamics_context, options):
         #               0 <= p_y <= C
         # for some arbitrary positive bound C, say 2.
 
-        C = 2.0
-        #epsilon = 1.0e-5
-        epsilon = 0.0
-        # Perturb the bounds for p_x by epsilon away from the actual bound.
-        X_0 = np.array([
-            rng.uniform(-np.sqrt(4/np.pi)+epsilon, np.sqrt(4/np.pi)-epsilon),
-            rng.uniform(0.0, C)
-        ])
+        #C = 2.0
+        ##epsilon = 1.0e-5
+        #epsilon = 0.0
+        ## Perturb the bounds for p_x by epsilon away from the actual bound.
+        #X_0 = np.array([
+            #rng.uniform(-np.sqrt(4/np.pi)+epsilon, np.sqrt(4/np.pi)-epsilon),
+            #rng.uniform(0.0, C)
+        #])
+        X_0 = random_embedding2_point(rng)
 
         #X_0 = np.array([4.53918797113298744e-01,-6.06738228528062038e-04,1.75369725636529949e+00])
 
@@ -1127,12 +1152,102 @@ def search (dynamics_context, options):
         print('got KeyboardInterrupt -- exiting program')
         sys.exit(0)
 
+## TODO: Come up with less generic name.
+#Sample = collections.namedtuple('Sample', ['initial2', 'qp_0', 'dt', 'max_time', 'objective', 't_min', 'max_abs_H', 'max_abs_J_minus_J_0', 'flow_curve_was_salvaged'])
+def make_sample (*, initial2, qp_0, dt, max_time, objective, t_min, max_abs_H, max_abs_J_minus_J_0, flow_curve_was_salvaged):
+    return (initial2, qp_0, dt, max_time, objective, t_min, max_abs_H, max_abs_J_minus_J_0, flow_curve_was_salvaged)
+
+def sample (dynamics_context, options, *, rng):
+    """
+    Program to search for orbits in the 2D embedding space:
+    -   Generate random points in the domain
+
+            -sqrt(4/pi) <= p_x <= sqrt(4/pi)
+                     -C <= p_y <= C
+
+        for some arbitrary positive constant C, say 2.  Due to a discrete symmetry in the system
+        (reflection), p_y can be taken to be nonnegative.  Thus the domain can be
+
+            -sqrt(4/pi) <= p_x <= sqrt(4/pi)
+                      0 <= p_y <= C
+
+    -   For each of these, compute the embedding qp_0 into phase space and use that as the initial
+        condition for the flow curve.  Use some fixed dt and max_time.
+    -   For each flow curve, compute the following values:
+        -   The objective function value (which could be NaN if the curve didn't go back toward itself)
+        -   The t_min value (which for a closed curve would be its period)
+        -   The upper bound on abs(H), or more detailed stats (mean and variance, or a histogram)
+        -   The upper bound on abs(J - J_0), or more detailed stats (mean and variance, or a histogram)
+        -   If it is [close to being] closed, then the order of its radial symmetry
+        -   The deviation of its z(t) function from a sine wave (for closed, this sometimes but not
+            always appears to be a sine wave, but this hasn't been checked rigorously).
+    -   Visualize these two functions to determine if they are continuous and/or have other structure
+        to them.  Perhaps there are regions which are continuous surrounding zeros the objective function.
+    """
+
+    if options.seed is None:
+        print('--seed must be specified.')
+        sys.exit(-1)
+
+    if not os.path.exists('shooting_method_3.sample/'):
+        os.mkdir('shooting_method_3.sample')
+
+    sample_v = []
+    try:
+        while True:
+            initial2 = random_embedding2_point(rng)
+            qp_0 = dynamics_context.embedding2(initial2)
+            dt = options.dt
+            max_time = options.max_time
+
+            try:
+                smo = ShootingMethodObjective(dynamics_context=dynamics_context, qp_0=qp_0, t_max=max_time, t_delta=dt)
+
+                flow_curve = smo.flow_curve()
+                objective = smo.objective()
+                t_min = smo.t_min()
+                abs_H_v = np.abs(vorpy.apply_along_axes(dynamics_context.H, (-2,-1), (flow_curve,)))
+
+                J_v = vorpy.apply_along_axes(dynamics_context.J, (-2,-1), (flow_curve,))
+                J_0 = J_v[0]
+                J_v -= J_0
+                abs_J_minus_J_0 = np.abs(J_v)
+
+                sample = make_sample(
+                    initial2=initial2,
+                    qp_0=qp_0,
+                    dt=dt,
+                    max_time=max_time,
+                    objective=objective,
+                    t_min=t_min,
+                    max_abs_H=np.max(abs_H_v),
+                    max_abs_J_minus_J_0=np.max(abs_J_minus_J_0),
+                    flow_curve_was_salvaged=smo.flow_curve_was_salvaged
+                )
+                print('recording sample {0}'.format(sample))
+            except vorpy.symplectic_integration.exceptions.SalvagedResultException as e:
+                print('caught exception "{0}" -- storing and continuing'.format(e))
+                sample = make_sample(
+                    initial2=initial2,
+                    qp_0=qp_0,
+                    dt=dt,
+                    max_time=max_time,
+                    objective=np.nan,
+                    t_min=np.nan,
+                    max_abs_H=np.nan,
+                    max_abs_J_minus_J_0=np.nan,
+                    flow_curve_was_salvaged=True
+                )
+
+            sample_v.append(sample)
+    except (Exception,KeyboardInterrupt) as e:
+        print('caught exception "{0}" -- saving results and exiting.'.format(e))
+        filename = 'shooting_method_3.sample/sample_v.seed:{0}.count:{1}.pickle'.format(options.seed, len(sample_v))
+        vorpy.pickle.try_to_pickle(data=sample_v, pickle_filename=filename, log_out=sys.stdout)
 
 if __name__ == '__main__':
     import matplotlib
     import matplotlib.pyplot as plt
-    import os
-    import sys
 
     # https://github.com/matplotlib/matplotlib/issues/5907 says this should fix "Exceeded cell block limit" problems
     matplotlib.rcParams['agg.path.chunksize'] = 10000
@@ -1150,7 +1265,9 @@ if __name__ == '__main__':
     rng = np.random.RandomState(options.seed)
 
     if options.search:
-        search(dynamics_context, options)
+        search(dynamics_context, options, rng=rng)
+    elif options.sample:
+        sample(dynamics_context, options, rng=rng)
     else:
         if not os.path.exists('shooting_method_3.custom_plot/'):
             os.mkdir('shooting_method_3.custom_plot')
