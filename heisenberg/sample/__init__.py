3a48127939917d0cd75dec493bba9166abaaef0d
Victor Dods <victor.dods@gmail.com>
2017-07-11 00:37:29 -0700

    Added multiprocessing capability to heisenberg.sample subprogram.

--- heisenberg/sample/__init__.py
+++ heisenberg/sample/__init__.py
@@ -1,8 +1,10 @@
+import concurrent.futures
 import heisenberg.library.shooting_method_objective
 import heisenberg.util
 import numpy as np
 import os
 import sys
+import traceback
 import vorpy
 import vorpy.pickle
 import vorpy.symplectic_integration.exceptions
@@ -13,6 +15,53 @@ subprogram_description = 'Samples a specified parameter space of initial conditi
 #Sample = collections.namedtuple('Sample', ['initial2', 'qp_0', 'dt', 'max_time', 'objective', 't_min', 'max_abs_H', 'max_abs_J_minus_J_0', 'flow_curve_was_salvaged'])
 def make_sample (*, initial2, qp_0, dt, max_time, objective, t_min, max_abs_H, max_abs_J_minus_J_0, flow_curve_was_salvaged):
     return (initial2, qp_0, dt, max_time, objective, t_min, max_abs_H, max_abs_J_minus_J_0, flow_curve_was_salvaged)
+
+def worker (args):
+    assert len(args) == 4
+    dynamics_context, initial2, dt, max_time = args[0], args[1], args[2], args[3]
+    qp_0 = dynamics_context.embedding(2)(initial2)
+
+    try:
+        # Use disable_salvage=True to avoid clogging up the place.
+        smo = heisenberg.library.shooting_method_objective.ShootingMethodObjective(dynamics_context=dynamics_context, qp_0=qp_0, t_max=max_time, t_delta=dt, disable_salvage=True)
+
+        flow_curve = smo.flow_curve()
+        objective = smo.objective()
+        t_min = smo.t_min()
+        abs_H_v = np.abs(vorpy.apply_along_axes(dynamics_context.H, (-2,-1), (flow_curve,)))
+
+        J_v = vorpy.apply_along_axes(dynamics_context.J, (-2,-1), (flow_curve,))
+        J_0 = J_v[0]
+        J_v -= J_0
+        abs_J_minus_J_0 = np.abs(J_v)
+
+        sample = make_sample(
+            initial2=initial2,
+            qp_0=qp_0,
+            dt=dt,
+            max_time=max_time,
+            objective=objective,
+            t_min=t_min,
+            max_abs_H=np.max(abs_H_v),
+            max_abs_J_minus_J_0=np.max(abs_J_minus_J_0),
+            flow_curve_was_salvaged=smo.flow_curve_was_salvaged
+        )
+        print('recording sample {0}'.format(sample))
+    except vorpy.symplectic_integration.exceptions.SalvagedResultException as e:
+        print('caught exception "{0}" -- storing and continuing'.format(e))
+        sample = make_sample(
+            initial2=initial2,
+            qp_0=qp_0,
+            dt=dt,
+            max_time=max_time,
+            objective=np.nan,
+            t_min=np.nan,
+            max_abs_H=np.nan,
+            max_abs_J_minus_J_0=np.nan,
+            flow_curve_was_salvaged=True
+        )
+
+    return sample
 
 def sample (dynamics_context, options, *, rng):
     """
@@ -46,58 +95,53 @@ def sample (dynamics_context, options, *, rng):
         print('--seed must be specified.')
         sys.exit(-1)
 
+    if options.sample_count <= 0:
+        print('--sample-count option, if present, must specify a positive integer.')
+        sys.exit(-1)
+
+    if options.max_workers <= 0:
+        print('--worker-chunksize option, if present, must specify a positive integer.')
+        sys.exit(-1)
+
+    if options.max_workers <= 0:
+        print('--max-workers option, if present, must specify a positive integer.')
+        sys.exit(-1)
+
     heisenberg.util.ensure_dir_exists(options.samples_dir)
 
     sample_v = []
-    try:
-        while True:
-            initial2 = heisenberg.util.random_embedding2_point(rng)
-            qp_0 = dynamics_context.embedding2(initial2)
-            dt = options.dt
-            max_time = options.max_time
 
+    if options.max_workers is None or options.max_workers > 1:
+        # Map the worker function over multiple processes.
+        with concurrent.futures.ProcessPoolExecutor(max_workers=options.max_workers) as executor:
             try:
-                smo = heisenberg.library.shooting_method_objective.ShootingMethodObjective(dynamics_context=dynamics_context, qp_0=qp_0, t_max=max_time, t_delta=dt)
+                sample_counter = range(options.sample_count)
+                sample_index = 1
+                for sample in executor.map(worker, ((dynamics_context, heisenberg.util.random_embedding2_point(rng), options.dt, options.max_time) for _ in sample_counter), chunksize=options.worker_chunksize):
+                    sample_v.append(sample)
+                    print('**************** saving sample {0} (out of {1}): {2}'.format(sample_index, options.sample_count, sample))
+                    sample_index += 1
+            except (Exception,KeyboardInterrupt) as e:
+                print('encountered exception of type {0} during sample; calling executor.shutdown(wait=True), saving results, and exiting.  exception was: {1}'.format(type(e), e))
+                print('stack:')
+                ex_type,ex,tb = sys.exc_info()
+                traceback.print_tb(tb)
+                executor.shutdown(wait=True)
+    else:
+        # Run the worker function in this single process.
+        try:
+            while True:
+                sample = worker((dynamics_context, heisenberg.util.random_embedding2_point(rng), options.dt, options.max_time))
+                sample_v.append(sample)
+        except (Exception,KeyboardInterrupt) as e:
+            print('encountered exception of type {0} during sample; saving results and exiting.  exception was: {1}'.format(type(e), e))
+            print('stack:')
+            ex_type,ex,tb = sys.exc_info()
+            traceback.print_tb(tb)
 
-                flow_curve = smo.flow_curve()
-                objective = smo.objective()
-                t_min = smo.t_min()
-                abs_H_v = np.abs(vorpy.apply_along_axes(dynamics_context.H, (-2,-1), (flow_curve,)))
+    print('saving results, and exiting.')
+    filename = os.path.join(options.samples_dir, 'sample_v.seed:{0}.count:{1}.pickle'.format(options.seed, len(sample_v)))
+    vorpy.pickle.try_to_pickle(data=sample_v, pickle_filename=filename, log_out=sys.stdout)
 
-                J_v = vorpy.apply_along_axes(dynamics_context.J, (-2,-1), (flow_curve,))
-                J_0 = J_v[0]
-                J_v -= J_0
-                abs_J_minus_J_0 = np.abs(J_v)
 
-                sample = make_sample(
-                    initial2=initial2,
-                    qp_0=qp_0,
-                    dt=dt,
-                    max_time=max_time,
-                    objective=objective,
-                    t_min=t_min,
-                    max_abs_H=np.max(abs_H_v),
-                    max_abs_J_minus_J_0=np.max(abs_J_minus_J_0),
-                    flow_curve_was_salvaged=smo.flow_curve_was_salvaged
-                )
-                print('recording sample {0}'.format(sample))
-            except vorpy.symplectic_integration.exceptions.SalvagedResultException as e:
-                print('caught exception "{0}" -- storing and continuing'.format(e))
-                sample = make_sample(
-                    initial2=initial2,
-                    qp_0=qp_0,
-                    dt=dt,
-                    max_time=max_time,
-                    objective=np.nan,
-                    t_min=np.nan,
-                    max_abs_H=np.nan,
-                    max_abs_J_minus_J_0=np.nan,
-                    flow_curve_was_salvaged=True
-                )
-
-            sample_v.append(sample)
-    except (Exception,KeyboardInterrupt) as e:
-        print('caught exception "{0}" -- saving results and exiting.'.format(e))
-        filename = os.path.join(options.samples_dir, 'sample_v.seed:{0}.count:{1}.pickle'.format(options.seed, len(sample_v)))
-        vorpy.pickle.try_to_pickle(data=sample_v, pickle_filename=filename, log_out=sys.stdout)
 
--- heisenberg/sample/__main__.py
+++ heisenberg/sample/__main__.py
@@ -19,6 +19,27 @@ op.add_option(
     type='str',
     help='Specify the directory that sample_v.*.pickle files will be read from.'
 )
+op.add_option(
+    '--sample-count',
+    dest='sample_count',
+    default=1000,
+    type='int',
+    help='Specify the number of samples to generate.  Default is 1000.'
+)
+op.add_option(
+    '--worker-chunksize',
+    dest='worker_chunksize',
+    default=1,
+    type='int',
+    help='Specify the chunksize for multiprocess worker delegation.  Default is 1.'
+)
+op.add_option(
+    '--max-workers',
+    dest='max_workers',
+    default=None,
+    type='int',
+    help='Optionally specify the max number of processes this program should use concurrently.  Default is the number of processors.'
+)
 
 options,args = op.parse_argv_and_validate()
 if options is None:
